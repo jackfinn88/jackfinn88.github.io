c1537ab83f979688e7c5655fea180e57bdd49fe5
jackfinn88 <finnjack@hotmail.co.uk>
2019-10-26 20:14:00 +0100

    Adding demo files

---
+++ ci328/js/entities.js
@@ -0,0 +1,123 @@
+class Control {
+
+    onUpdate(sprite) {
+
+    }
+}
+
+class EntityFactory {
+    constructor(spriteName) {
+        const group = game.physics.add.group({
+            defaultKey: spriteName
+        });
+
+        this.group = group;
+    }
+
+    spawnAsBullet(x, y) {
+        if (!this.nextBulletTime) {
+            this.nextBulletTime = 0;
+        }
+
+        //  To avoid them being allowed to fire too fast we set a time limit
+        if (game.time.now > this.nextBulletTime) {
+
+            const sprite = this.group.create(x-1, y-2)
+            this.setUpEntity(sprite);
+            
+            sprite.setOrigin(0.5, 0.5);
+            
+            sprite.setVelocity(0, -250);
+            this.nextBulletTime = game.time.now + 200;
+        }
+    }
+
+    spawnAsEnemy(x, y) {
+        const sprite = this.group.create(x, y + 30);
+        
+        this.setUpEntity(sprite);
+        
+        sprite.setVelocity(Phaser.Math.Between(35, 55), Phaser.Math.Between(45, 100));
+
+        sprite.setOrigin(1.0, 1.0);
+
+        sprite.addControl(new EnemyControl());
+    }
+    
+    setUpEntity(sprite) {
+        sprite.controls = [];
+        sprite.addControl = (control) => { sprite.controls.push(control); }
+        sprite.updateControls = () => { sprite.controls.forEach(control => control.onUpdate(sprite)); }
+    }
+
+    updateAllExists() {
+        this.group.children.iterate(function (sprite) {
+            if (sprite)
+                sprite.updateControls();
+        })
+    }
+
+    destroyAllExists() {
+        this.group.children.iterate(function (sprite) {
+            sprite.destroy();
+        })
+    }
+}
+
+class EnemyControl extends Control {
+
+    onUpdate(sprite) {
+        if (sprite.x + sprite.width + sprite.width >= phaser.config.width) {
+            sprite.setVelocityX(-Phaser.Math.Between(35, 55));
+        }
+        
+        if (sprite.x <= 0) {
+            sprite.setVelocityX(Phaser.Math.Between(35, 55));
+        }
+        
+        if (sprite.y > phaser.config.height + 40) {
+            sprite.destroy();
+            world.numEnemies--;
+        }
+    }
+}
+
+class World {
+    constructor(game) {
+        this.bg = game.add.image(0, 0, 'background_img');
+        this.bg.setOrigin(0, 0);
+
+        this.player = new Player();
+        this.player.onDeath(gameOver);
+
+        this.bulletFactory = new EntityFactory('bullet_img');
+        this.enemyFactory = new EntityFactory('enemy_sp');
+        
+        this.numEnemies = 0;
+    }
+
+    spawnEnemy(x, y) {
+        this.enemyFactory.spawnAsEnemy(x, y);
+        this.numEnemies++;
+    }
+
+    spawnBullet(x, y) {
+        this.bulletFactory.spawnAsBullet(x, y);
+    }
+
+    update() {
+        //  Scroll the background, reset it when it reaches the bottom
+        this.bg.y += 2;
+
+        if (this.bg.y >= 0) {
+            this.bg.y = -phaser.config.height;
+        }
+
+        this.enemyFactory.updateAllExists();
+    }
+
+    cleanup() {
+        this.enemyFactory.destroyAllExists();
+        this.bulletFactory.destroyAllExists();
+    }
+}
---
+++ ci328/js/systems.js
@@ -0,0 +1,56 @@
+class Audio {
+    constructor() {
+        this.intro = game.sound.add('intro');
+        //this.intro.play();
+        this.bg = game.sound.add('bg', true);
+        //this.bg.play();
+        this.explode = game.sound.add('explode');
+        this.fly = game.sound.add('fly');
+        this.shoot = game.sound.add('shoot');
+    }
+}
+
+class Input {
+    constructor() {
+        this.keyMap = new Map();
+    }
+
+    add(key, action) {
+        this.keyMap.set(game.input.keyboard.addKey(key), action);
+    }
+
+    update() {
+        for (const [key, action] of this.keyMap.entries()) {
+            if (key.isDown) {
+                action();
+            }
+        }
+    }
+}
+
+class UI {
+    constructor() {
+        this.startGameText = game.add.text(phaser.config.width / 2, phaser.config.height / 2, 'Click to Start', {
+            font: '30px Arial',
+            fill: '#fff'
+        });
+        this.startGameText.setOrigin(0.5, 0.5);
+
+        this.scoreText = game.add.text(10, 10, 'Score: 0', {
+            font: '34px Arial',
+            fill: '#fff'
+        });
+    }
+
+    updateScoreText(newScore) {
+        this.scoreText.setText('Score: ' + newScore);
+    }
+
+    showStartText() {
+        this.startGameText.visible = true;
+    }
+
+    disableStartText() {
+        this.startGameText.visible = false;
+    }
+}

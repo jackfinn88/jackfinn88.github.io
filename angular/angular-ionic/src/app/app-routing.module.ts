812f8a462d9f607905626e8bd6a083bc6cc7d4e0
jackfinn88 <finnjack@hotmail.co.uk>
2020-07-20 17:19:31 +0100

    Added page components

--- angular/angular-ionic/src/app/app-routing.module.ts
+++ angular/angular-ionic/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { IonicComponent } from './components/pages/ionic/ionic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'ionic', component: IonicComponent },
+
+  // otherwise redirect to home
+  { path: '**', redirectTo: '' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/angular-ionic/src/app/app.module.ts
+++ angular/angular-ionic/src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { IonicModule } from '@ionic/angular';
+import { HomeComponent } from './components/pages/home/home.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { IonicComponent } from './components/pages/ionic/ionic.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    IonicComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+    IonicModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
